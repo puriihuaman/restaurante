b5f2b7bf3364de2718eeb8d0ab0aa195eeb38125
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-09-14 13:15:45 -0500

    feat(ui): add print and return buttons
    add button to print the ticket
    
    implement the function to return to the index

--- src/app/components/ticket/ticket.component.ts
+++ src/app/components/ticket/ticket.component.ts
@@ -1,4 +1,4 @@
-import { AsyncPipe, CurrencyPipe } from "@angular/common";
+import { AsyncPipe, CurrencyPipe, DatePipe, NgClass } from "@angular/common";
 import { Component, inject, type OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { OrderComponent } from "@components/order/order.component";
@@ -8,35 +8,46 @@ import { OrderService } from "@services/order.service";
 @Component({
 	selector: "app-ticket",
 	standalone: true,
-	imports: [AsyncPipe, CurrencyPipe, OrderComponent],
+	imports: [AsyncPipe, CurrencyPipe, NgClass, DatePipe, OrderComponent],
 	templateUrl: "./ticket.component.html",
 	styleUrl: "./ticket.component.scss",
 })
 export class TicketComponent implements OnInit {
 	private orderService: OrderService = inject(OrderService);
-	public currentOrder!: Order;
 	private route: ActivatedRoute = inject(ActivatedRoute);
 	private router: Router = inject(Router);
 	private orderCode: Order["code"] | null = null;
 
+	public currentOrder!: Order;
+	public currentDate!: Date;
+	public toPrint: boolean = false;
+
 	ngOnInit(): void {
 		this.orderCode = this.route.snapshot.paramMap.get("code");
+
 		if (this.orderCode !== null) {
 			const existingOrder: Order | undefined =
 				this.orderService.verifyExistenceOfTheOrder(this.orderCode);
 
 			if (existingOrder) {
 				this.currentOrder = existingOrder;
-				this.printTicket();
+				this.currentDate = new Date();
 			} else {
-				this.router.navigate(["/"]);
+				this.returnHome();
 			}
 		}
 	}
 
-	private printTicket(): void {
+	public printTicket(): void {
+		this.toPrint = true;
+		console.log(this.toPrint);
 		setTimeout((): void => {
 			window.print();
-		}, 100);
+			this.toPrint = false;
+		}, 200);
+	}
+
+	public returnHome(): void {
+		this.router.navigate(["/"]);
 	}
 }
